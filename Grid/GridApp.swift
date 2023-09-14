6fed15fba150f897a2fab97252d555c699f86b61
Alexey Turulin <repakuku@icloud.com>
2023-09-14 17:34:58 -0400

    Initial Commit

---
+++ Grid/GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridApp.swift
+//  Grid
+//
+//  Created by Alexey Turulin on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
