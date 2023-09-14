2506672c1c566a9729011802eb70534f55967ca6
Alexey Turulin <repakuku@icloud.com>
2023-09-14 19:52:54 -0400

    working with tab view

--- Grid/ContentView.swift
+++ Grid/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let circle = ".circle"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            TabView {
+                CustomGridView(items: Array(1...50), columns: 3) { item in
+                    Image(systemName: item.formatted() + circle)
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+                .tabItem {
+                    Image(systemName: "hand.point.up.left.fill")
+                    Text("Custom Grid")
+                }
+                
+                VerticalGridView()
+                    .tabItem {
+                        Image(systemName: "hand.point.down")
+                        Text("Vertical Grid")
+                    }
+                
+                HorizontalGridView()
+                    .tabItem {
+                        Image(systemName: "hand.point.right.fill")
+                        Text("Horizontal Grid")
+                    }
+            }
         }
-        .padding()
     }
 }
 
