ef80df923acf8ddaa499440a2d0c0eb6d5eff4dd
Alexey Turulin <repakuku@icloud.com>
2023-09-14 19:30:00 -0400

    working with horizontal grid

---
+++ Grid/HorizontalGridView.swift
@@ -0,0 +1,31 @@
+//
+//  HorizontalGridView.swift
+//  Grid
+//
+//  Created by Alexey Turulin on 9/14/23.
+//
+
+import SwiftUI
+
+struct HorizontalGridView: View {
+    private let dataSet = (1...10)
+    private let rows = [ GridItem() ]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: rows, spacing: 20) {
+                ForEach(dataSet, id: \.self) { item in
+                    Image(systemName: item.formatted() + ".circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+            }
+        }
+    }
+}
+
+struct HorizontalGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        HorizontalGridView()
+    }
+}
